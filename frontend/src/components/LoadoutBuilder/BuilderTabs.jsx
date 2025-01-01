5864c0756a67348c54a9010a26ad9dc3eb852848
WildChargerTV <46546991+WildChargerTV@users.noreply.github.com>
2025-01-01 15:00:47 -0700

    Optimized BuilderTabs selection code, updated docs

--- frontend/src/components/LoadoutBuilder/BuilderTabs.jsx
+++ frontend/src/components/LoadoutBuilder/BuilderTabs.jsx
@@ -8,14 +8,20 @@ import { changeTab } from '../../store/builder';
 
 /**
  * Renders an EVERSPACE-style tab navigation menu for the Loadout Builder. Changing the tab updates 
- * the Redux state accordingly.
+ * the Redux state accordingly. This component will not be visible if the `LoadoutBuilderMain`
+ * component is also loaded.
  * 
- * Tabs:
+ * **This component DOES NOT link to the body contents of the Loadout Builder.** This is also
+ * handled in `LoadoutBuilderMain`. This component only manages the active tab and communicates it
+ * to the Redux state.
+ * 
+ * At present, the Loadout Builder consists of the following tabs:
  * - `0`: Ship Selection
  * - `1`: Enhancement Selection
- * - `2`: Equipment Selection
+ * - `2`: Equipment Selection & Customization
  * @component `BuilderTabs`
- * @param {{ isLoaded: boolean; }}
+ * @requires {@linkcode changeTab}
+ * @param {{ isLoaded: boolean }}
  * @returns {null | ReactElement}
  */
 export default function BuilderTabs({ isLoaded }) {
@@ -25,24 +31,31 @@ export default function BuilderTabs({ isLoaded }) {
 
     /** When one of the inactive tabs is clicked, make that tab active. */
     const onClick = (event) => {
-        const currentActive = document.getElementsByClassName('active')[1];
-        if(currentActive !== event.target) {
-            const btnNum = Number(event.target.id.split('-')[2]);
-            dispatch(changeTab(btnNum));
-        }
+        const currId = Number(event.target.id.split('-')[2]);
+        if(currId !== tabId)
+            dispatch(changeTab(currId));
     }
 
     /** Set the active tab to the one defined in the Redux store. */
     useEffect(() => {
+        // Return early if the tab menu is not yet loaded.
         if(!isLoaded) return;
-        const currentActive = document.getElementsByClassName('active')[1];
-        if(currentActive) currentActive.className = '';
-        document.getElementById(`builder-tab-${tabId}`).className = 'active';
-    }, [tabId, isLoaded]);
 
+        // Iterate through the three tab buttons, and set their active status accordingly.
+        for(const tab of Object.values(document.getElementById('builder-tabs').children)) {
+            const currId = Number(tab.id.split('-')[2]);
+            tab.className = currId === tabId ? 'tab-active' : '';
+        }
+    }, [isLoaded, tabId]);
+
+    /** 
+     * In order for the Loadout Builder's Create mode to function properly, the user cannot be
+     * allowed to select Enhancements or Equipment until they have both chosen a ship & chosen
+     * (or denied) a preset loadout for that ship.
+     */
     const disabled = mode === 'create' && (shipId === null || shipPreset === null);
     
-    /** Return tab menu content. Enhancement/Equipment tabs are disabled until a ship is chosen. */
+    /** Return tab menu content. */
     return isLoaded && (<div id='builder-tabs'>
         <button id='builder-tab-0' onClick={onClick}>Ships</button>
         <button id='builder-tab-1' onClick={onClick} disabled={disabled}>Enhancements</button>
