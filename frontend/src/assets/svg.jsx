9be9ee5d5b25dd89d88a2e76b3cc75e86404a43c
WildChargerTV <46546991+WildChargerTV@users.noreply.github.com>
2025-01-10 21:06:54 -0700

    Equipment Controls are now built-in SVGs

---
+++ frontend/src/assets/svg.jsx
@@ -0,0 +1,96 @@
+// * src/frontend/assets/svg.jsx
+
+export function AddEquipIcon({ height }) {
+    return (<svg
+        id='menu-add-equip'
+        width={height}
+        height={height}
+        viewBox='0 0 20 20'
+        xmlnsXlink='http://www.w3.org/1999/xlink'
+        xmlns='http://www.w3.org/2000/svg'
+        xmlns:svg='http://www.w3.org/2000/svg'
+      >
+        <defs id='defs1' />
+        <g id='g1'>
+        <image
+            width={20}
+            height={20}
+            preserveAspectRatio='none'
+            xlinkHref='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAADHUlEQVQ4Ea1VTUtbQRTNM0/z0SZp&#10;EkND1ShkEbEILRKtNFCRKHTXhT/BvYgbSyGaRVvBhfo7xEXpSqEqVNui0IUi/YDQhZik1TQx1qj5&#10;7Dlj5vU9rF31wuTeuXPmvDt37twoTU1NJp0EnU6nF/OKzvcv05zP5zMAJCRItVgs0g5NTk4+tdvt&#10;PjhIWKsvSC1xSt2gNhcKhcOZmZmXsL/Q34CITBidOzs7H3p7ex/Al8eocpGSyWRyyWQynUql0pCj&#10;Wq0m16jzfX19/dxLDnIpVqu1Ix6PPx8cHBwAoAXDIOFw+BYcgbozs7W1dWAAXE4O1tbW1qempp6p&#10;YPU7HI7mlZWVzb8ATcvLy6+Hh4cjXEOEuUAgcBdmUo+dnp7edLvdzeRSq9Vq+QJis9kMtyM34Igu&#10;aTc2NjqAd8i51NxLDnKpcDLptQaIBOg1kv5Lzk8hlUolu7CwIF1SN4BM8JBQiEz2+Pj4TTjcGKcY&#10;5qWlJX3k3NQxNjZ2Ds1Iz+bm5n5C84LE7WuEJycnx7FYTN3Y2Hjr8/kCpVLpAtFU29vbPQAL8fv9&#10;N/b29l4hmgoOhIqznPf09ERHR0cPW1tbWW4mjRCbi5jbUAb3VFVzCyL5g0egdnV1+eWcGsRtRQhM&#10;Ua9X8nZNKvUcBltRxEkFGRe0UMxmM59MAfX0zuv1tpXLZX64FgqF7mBuJxgXWUIRHyDfReCtPDJS&#10;8w3aimVjDlGLLiS4Eo1GH2GRxcxLURcXF9+MjIyEYbMOT/GansD8hMFLKWDPGbTIH/SfCBG6+ML8&#10;/HwZ/iMuUpBPrVEAwhR9B4Y5Y1OQoiBqcWwemUQKbk6+UQkSGtWvFTJegxNHdU5MTKT1oNnZ2Wr9&#10;Y4qKS2DHsWSz2WM9SNpoDimUFJ+baX9/P42NbB4GQfEXPR6Pi1wq+lkaG46GhoYGDKj6BDl7DFOW&#10;Sm57e1t7OTr8w9XV1XVyKS0tosF0oqDfJxKJH7u7ux8BtGGInOJjORY5N+O4bCbu+vGYs7Pu7u77&#10;wWDwdiQS6cf8s4LbJZZyXYO9XL36yw/KBvsC9ldClP/9F/Abl3pJgeWmy2gAAAAASUVORK5CYII=&#10;'
+            id='image1'
+        />
+        </g>
+    </svg>);
+}
+
+export function RemoveEquipIcon({ height }) {
+    return (<svg
+        id='menu-remove-equip'
+        width={height}
+        height={height}
+        viewBox="0 0 20 20"
+        sodipodi:docname="menu-remove-equip.png"
+        xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+        xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:svg="http://www.w3.org/2000/svg"
+    >
+        <defs id="defs1" />
+        <sodipodi:namedview
+        id="namedview1"
+        pagecolor="#ffffff"
+        bordercolor="#000000"
+        borderopacity={0.25}
+        inkscape:showpageshadow={2}
+        inkscape:pageopacity={0}
+        inkscape:pagecheckerboard={0}
+        inkscape:deskcolor="#d1d1d1"
+        />
+        <g inkscape:groupmode="layer" inkscape:label="Image" id="g1">
+        <image
+            width={20}
+            height={20}
+            preserveAspectRatio="none"
+            xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAADUUlEQVQ4Ea1VXUiTURj+vvm5TdMh&#10;bdJQL5JlivlDgdOakPlzEy2JwqgbL5IILyYJis0/vEgFEYPAW0FIghgYdhPKJrhwKDjQGzdaXkyb&#10;qUmb003bZs/7zWOfInVRL7y85zzvOc/e8z7fOePlcjknMZ1KpVJjHpVgfxomBAKB71jgYYsEhULB&#10;xrltbW0vkpOT0wFEGPiXKOzt7W329/f3YZ2L1spQEQfPW1xcdOj1+jJggd3dXf/29jb98pkGkiDy&#10;W0j6S0tLy2gvcRAXr1QqL/b09LysrKy8hQUZjKGkpEQ5NDQUZnNpBCHX3t6unJ+fZ/mvVqt1uru7&#10;2yyAVZuamqqZnJy00yaz2Zw4Pj4+7fV6cwoLC6sALUnJaDwwMPB+dXXVgOruYOowmUx2cKiJSxaL&#10;xSL7MJ7nI3DOYrF8rK2tvZGVlZU+MzNjjUajRXCOeXNz81hDQ8PdzMxM9crKyqzf78/A8X+AIkxc&#10;MvzCIblMJkuAcx6P5zPmohUUFGgcDof14OAgB861tra+6ezsfMTyLpfL+xOWCAMW52FJ9DIJzkH1&#10;p01NTa8Znp+fr56YmPiAVrzr6Oh4zHD0bBkt0Q8ODm5qtVr61HjKUYWiodxDOEeenZ1tkpLW1NRc&#10;7urqesDW2mw2V1VVVSVEW0eBHDmzY0IGsEikdXV1/WzOInrsxBdxDWQ+hiHScUUT2OCsqNFo0k7j&#10;UDJZEARtS0vLF5aD6mz4+8jHyNFgZ2dnbHh4+NlpHMfPdTqdjqSkpCI4Rw4T+0eD4yNDYZ5UJg8G&#10;g2+lao6Ojn5qbGw8Fgrqp0N9Gyq9AufiIhOdhDAcDu/B6RpaoObDeJrjSM36+vp72ERCvWI41D/v&#10;drsXIpFIxtra2gZwsY9UIZXLH8aNrRcj1HSTmrgJm0eJ51L1QUbyit8fosgj4Ij04ihCodA+bfL5&#10;fPdHRkYmiouLL0HNit7e3m+ES8zU19d3zmg03jQYDLdxu3xpMFAoiUvAe7YOAbaqq6srJJuMdNXm&#10;5uYk0InhExTATU1NiSBIDfQ4EBePO0lgnt1un8W121haWlpAL2M4TiwlJUV1guZognsbwse8j7wc&#10;t+WqTqe7UF5efh3pZR6vBNvzLw9sL0jcRMT/77+AX1Iugq2rRm8QAAAAAElFTkSuQmCC&#10;"
+            id="image1"
+        />
+        </g>
+    </svg>);
+}
+
+export function AddModIcon({ height }) {
+    return (<svg
+        id='menu-add-mod'
+        width={height}
+        height={height}
+        viewBox="0 0 20 20"
+        sodipodi:docname="menu-add-mod.png"
+        xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+        xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:svg="http://www.w3.org/2000/svg"
+    >
+        <defs id="defs1" />
+        <sodipodi:namedview
+        id="namedview1"
+        pagecolor="#ffffff"
+        bordercolor="#000000"
+        borderopacity={0.25}
+        inkscape:showpageshadow={2}
+        inkscape:pageopacity={0}
+        inkscape:pagecheckerboard={0}
+        inkscape:deskcolor="#d1d1d1"
+        />
+        <g inkscape:groupmode="layer" inkscape:label="Image" id="g1">
+        <image
+            width={20}
+            height={20}
+            preserveAspectRatio="none"
+            xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAC6ElEQVQ4Ea1V3UuTYRTfu/ed+3KL&#10;ypghrGgXNZpeTK0FE8Q5kCL/Aq+76SKIlotdiIi0wCvvuhCGZN6I3uTN5siJMYUU05sF7mrS1sc+&#10;2NzQd1/9zkvPmrZpQQeenfOcj99z3nPO84xraWmR1ZFJr9dfxr5cpztL5LPZbBIOUeYkKJVKJt90&#10;u90vNBrNFSgIsMoMTTgHPV8oFL57vd6XkD+TH9fR0UH81vr6enh/f//r7u7udrFYJF0Fi4KaEdnF&#10;np4eu8lkMtjtdhv2EU6lUl0fHx+fHBgY6IdCQgf/J+I47kswGHw/NjbmEVCzdp1O1+b3+z8QyvT0&#10;dLvP53ujUCguViqVprWUy+V8Pp9PIcQZDoffGY3Ga4QlIKh0DFKr1QoCFATBODQ05CD5bwjZXfV4&#10;PGXCICwBQVT8KgxSvarV83rx+5hMJlOiWJ7nazgkMGINSEYikRi6falUKok4QDw4OEh2d3ffaG1t&#10;VTHnOn4ig3pAycflckUtFssdbHTlcvkQPD08PDw5Nzf3uA6kqfgHIHlOTU0lwGjJlpeXHy0sLDxB&#10;k3jan0fysxxCodDzQCDwmoGtrq7G5+fnP54V0xRwbW3NvbS09IoFx+PxPGb1IUbkE9M14g0Bkdno&#10;4uIiXSeJYrHYsdVqHTQYDFtms7mN6Rvx+hpKQ7yzs/MUmXmZMzIT+/r6HuDOb5AO06VmNjwsFE9J&#10;1TpNChoXjs1fKpVKoLsyzJYM41Lo7e29D3sISyJWT9rgMhATMdAEKOEIuEL04ijT6fQRWW0221uH&#10;wyHgBXk2MjLi1mq1NTCyT0xMjM7MzNyGKBdFMYX5TOIQLWEQloD3LJHL5X44nc5+CiCCPAs2i45K&#10;+1M/W9jTqhG+ZnBlZSVIWCeer2g0+m1vb28bnyDQ3cRLpEHNTs8ffR5bR52dndZfz9c96CNyoMqw&#10;Il1dXbbNzc0wlHqkrkR9dA3AKCuqFTWC6n8BMRuIvUsYhMX977+An5CiKYKFW1FkAAAAAElFTkSu&#10;QmCC&#10;"
+            id="image1"
+        />
+        </g>
+    </svg>);
+}
