8dcfc374ee0cdffe9ea6d07aab57299b9533be6e
WildChargerTV <46546991+WildChargerTV@users.noreply.github.com>
2025-01-12 10:13:02 -0700

    New routes to get a single user/user's loadouts

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -2,10 +2,11 @@
 
 // Node Module Imports
 const bcrypt = require('bcryptjs');
+const { Op } = require('sequelize');
 // Local Module Imports
-const { User } = require('../../db/models');
+const { Loadout, User } = require('../../db/models');
 const { setTokenCookie } = require('../../utils/auth');
-const { devLog } = require('../../utils/devLogger');
+const { devLog, devWarn, devErr } = require('../../utils/devLogger');
 const { validateSignup } = require('../../utils/validation');
 // Chalk Color Aliases
 const { green } = require('chalk');
@@ -20,6 +21,73 @@ const PATH = 'routes/api/users.js';
  * 1. `POST /api/users`: User Signup
  */
 const users = require('express').Router();
+
+/**
+ * GET /api/users/:userId
+ */
+users.get('/:userId', async (req, res, next) => {
+    const { userId } = req.params;
+
+    let user;
+    try {
+        user = await User.findByPk(userId)
+        .then((result) => result.toJSON());
+        devLog(PATH, `User ${userId} found ` + green('successfully'));
+    } catch(err) {
+        devErr(PATH, err.message, err);
+        return next(err);
+    }
+    
+    return res.json(user);
+})
+
+/**
+ * GET /api/users/:userId/loadouts
+ */
+users.get('/:userId/loadouts', async (req, res) => {
+    const { userId } = req.params;
+
+    let user;
+
+    const query = {
+        attributes: [
+            'id', 'name', 'shipId', 'enhancements', 'primaryWeapons', 'secondaryWeapons', 'devices', 
+            'consumables', 'createdAt', 'updatedAt'
+        ],
+        include: [{ model: User }],
+        order: [['createdAt', 'DESC']],
+        where: {
+            userId: { [Op.eq]: userId }
+        }
+    };
+
+    const list = await Loadout.findAll(query)
+    .then(async (result) => {
+        const arr = [];
+
+        for await (const loadout of result) {
+            const json = loadout.toJSON();
+            if(json.id === null) continue;
+
+            for(let key in json)
+                if(typeof json[key] === 'string' && !['name'].includes(key))
+                    json[key] = JSON.parse(json[key]);
+            
+            if(!user)
+                user = loadout.User;
+
+            delete loadout.User;
+            arr.push(json);
+        }
+
+        arr.length > 0
+        ? devLog(PATH, `Retrieved ${arr.length} loadouts belonging to user ID ${userId}` + green(' successfully'))
+        : devWarn(PATH, `Retrieval was successful; however, user ID ${userId} has no loadouts.`)
+        return arr;
+    });
+
+    return res.json({ list, user });
+});
 
 /** 
  * POST /api/users
