58a6a68f53d5525a90a1871ecabc29aab7ad1ecb
WildChargerTV <46546991+WildChargerTV@users.noreply.github.com>
2025-01-13 18:31:06 -0700

    No more deleting the database.

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -9,14 +9,14 @@ const { sequelize } = require('./db/models');
 
 // Force drop the schema if it exists. If not, create it.
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
-    // if(!data.includes(process.env.SCHEMA))
-    //     await sequelize.createSchema(process.env.SCHEMA);
-  if(data.includes(process.env.SCHEMA)) {
-    console.log('psql-setup-script: schema found, dropping');
-    await sequelize.dropSchema(process.env.SCHEMA);
-    await sequelize.createSchema(process.env.SCHEMA);
-  } else {
-    console.log('psql-setup-script: schema not found, creating');
-    await sequelize.createSchema(process.env.SCHEMA);
-  }
+    if(!data.includes(process.env.SCHEMA))
+        await sequelize.createSchema(process.env.SCHEMA);
+//   if(data.includes(process.env.SCHEMA)) {
+//     console.log('psql-setup-script: schema found, dropping');
+//     await sequelize.dropSchema(process.env.SCHEMA);
+//     await sequelize.createSchema(process.env.SCHEMA);
+//   } else {
+//     console.log('psql-setup-script: schema not found, creating');
+//     await sequelize.createSchema(process.env.SCHEMA);
+//   }
 });
