563cbf7ad3030147eab213354282dfd8a5d34320
WildChargerTV <46546991+WildChargerTV@users.noreply.github.com>
2025-01-16 20:50:31 -0700

    Added Demo Logins. Only allowed on the site when I say so.

--- frontend/src/components/Navigation/LoginModal.jsx
+++ frontend/src/components/Navigation/LoginModal.jsx
@@ -23,8 +23,54 @@ export default function LoginModal() {
     const [password, setPassword] = useState('');
     const [disabled, setDisabled] = useState(true);
     const [errors, setErrors] = useState({});
+    const [demoLoginWarning, setDemoLoginWarning] = useState(false);
     // Context Hooks
     const { closeModal } = useModal();
+
+    /** 
+     * Manual flag to allow demo logins on the site. 
+     * ! This is only intended for use where needed, and should not be enabled by default.
+     */
+    const showDemoLogin = true;
+
+    /** Perform a demo login, but only after showing a warning. */
+    const demoLogin = (event) => {
+        // Prevent a redirect/refresh.
+        event.preventDefault();
+        // Clear the Errors object.
+        setErrors({});
+
+        // If the warning has not been displayed yet, display it, and do not dispatch the login.
+        if(!demoLoginWarning) {
+            setErrors({ message: <>
+                <span className='red'>WARNING:</span> The Demo Log In is 
+                a <span className='yellow'>temporary</span> functionality meant for very specific
+                purposes. Most of the time, it will not be enabled, and you will need to sign in
+                with your own credentials. 
+                <br /><br />
+                You&apos;ll log in as Maurice, and have as much access to ESBuilder as any other
+                user account; however, loadouts you create as Maurice may be modified or deleted 
+                by anyone else that utilizes this demo login.
+                <br /><br />
+                If you understand these risks and want to proceed with the demo login, please click
+                the Demo button again.
+            </> });
+            return setDemoLoginWarning(true);
+        } 
+        // If the warning has been displayed, allow the demo login to proceed and close the Modal.
+        else {
+            setDemoLoginWarning(false);
+            return dispatch(sessionActions.login({ 
+                credential: 'Maurice', 
+                password: 'I|5F6!T7Ad' 
+            })).then(async (res) => res.ok && closeModal())
+            .catch(async (res) => {
+                const data = await res.json();
+                if(data?.errors) 
+                    setErrors(data.errors);
+            });
+        }
+    };
     
     /** Attempt to dispatch the login attempt, and close the Modal if no errors were found. */
     const onSubmit = (event) => {
@@ -38,7 +84,8 @@ export default function LoginModal() {
             .then(async (res) => res.ok && closeModal())
             .catch(async (res) => {
                 const data = await res.json();
-                if(data?.errors) setErrors(data.errors);
+                if(data?.errors) 
+                    setErrors(data.errors);
             });
     };
 
@@ -83,10 +130,18 @@ export default function LoginModal() {
                 required 
             />
 
-            {/* Submit Button - disabled unless requirements (described above) are met */}
-            <button type='submit' disabled={disabled}>
-                Log In <span className='site-text-icon'><PiMouseLeftClickFill /></span>
-            </button>
+            <div className='modal-controls'>
+
+                {/* Submit Button - disabled unless requirements (described above) are met */}
+                <button type='submit' disabled={disabled}>
+                    Log In <span className='site-text-icon'><PiMouseLeftClickFill /></span>
+                </button>
+                {/* Demo Login Button - only visible if manual flag is set */}
+                {showDemoLogin && <button onClick={demoLogin}>
+                    Demo <span className='site-text-icon'><PiMouseLeftClickFill /></span>
+                </button>}
+            </div>
+            
         </form>
     </>);
 }
